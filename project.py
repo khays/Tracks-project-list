95d9120920669574fa7af85ac8980b667aff5584
Kelly Hays <khays@myra.com>
2013-04-22 21:57:02 -0700

    Moved code around a bit, nothing major

--- project.py
+++ project.py
@@ -7,7 +7,6 @@ import ConfigParser
 # Get variables from the config file
 config = ConfigParser.RawConfigParser()
 config.read('project.conf')
-name = config.get('General Information', 'yourname')
 projectsURL = config.get('General Information', 'projectsURL')
 exportName = config.get('General Information', 'outputfile')
 
@@ -18,23 +17,30 @@ file.close()
 
 root = ET.fromstring(data)
 
-#def formatProject(item):
-    #return item
 
-priority = 1
 
-now = date.today()
-header = '# %s\'s projects as of %s\n\n' % (name,now)
-with open(exportName, 'w') as myfile:
-    myfile.write(header)
+def createHeader():
+    name = config.get('General Information', 'yourname')
+    now = date.today()
+    header = '# %s\'s projects as of %s\n\n' % (name,now)
+    with open(exportName, 'w') as myfile:
+        myfile.write(header)
 
-for item in root.findall('channel/item'):
-    title = item.find('title').text
-    with open(exportName, "a") as myfile:
-        number = "%i. " % (int(priority))
-        myfile.write(number)
-        myfile.write(title)
-        myfile.write('\n')
-    priority = priority + 1
+def createProjectList(xml):
+    priority = 1 # set the variable
 
-print 'File should be saved, take a look at\n\n%s\n' % exportName
+    # Loop through xml object
+    for item in root.findall('channel/item'):
+        title = item.find('title').text
+        with open(exportName, "a") as myfile:
+            number = "%i. " % (int(priority))
+            myfile.write(number)
+            myfile.write(title)
+            myfile.write('\n')
+        priority = priority + 1
+
+
+    print 'File should be saved, take a look at\n\n%s\n' % exportName
+
+createHeader()
+createProjectList(root)
